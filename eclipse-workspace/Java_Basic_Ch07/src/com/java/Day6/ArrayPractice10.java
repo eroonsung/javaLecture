6c1d706f70720e82d6c4bcb66836ad70d1390f56
Eroon Sung <sung7290@gmail.com>
2021-05-24 17:30:04 +0900

    20210520

--- eclipse-workspace/Java_Basic_Ch07/src/com/java/Day6/ArrayPractice10.java
+++ eclipse-workspace/Java_Basic_Ch07/src/com/java/Day6/ArrayPractice10.java
@@ -1,8 +1,22 @@
 package com.java.Day6;
 
+import java.util.Scanner;
+
 public class ArrayPractice10 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double[] classAvg = new double[] {85.6, 79.5, 83.1, 80.0, 78.2, 75.0};
+		System.out.println("첫번째 반을 입력하세요.");
+		int class1 = scan.nextInt();
+		System.out.println("두번째 반을 입력하세요.");
+		int class2 = scan.nextInt();
+		
+		double sum = classAvg[class1-1]+classAvg[class2-1];
+		System.out.println("첫번째 반 평균점수 : "+ classAvg[class1-1]);
+		System.out.println("두번째 반 평균점수 : "+ classAvg[class2-1]);
+		System.out.println("두 반 평균점수의 합 : "+ sum);
 		
 
 	}
--- eclipse-workspace/Java_Basic_Ch07/src/com/java/Day6/ArrayPractice9.java
+++ eclipse-workspace/Java_Basic_Ch07/src/com/java/Day6/ArrayPractice9.java
@@ -1,10 +1,29 @@
 package com.java.Day6;
 
+import java.util.Scanner;
+
 public class ArrayPractice9 {
 
 	public static void main(String[] args) {
-		String[] strArray = new String[6];
+		Scanner scan = new Scanner(System.in);
 		
+		char[] charArray = {'P','R','P','G','R','A','M','M','I','N','G'};
+		
+		int count = 0;
+		
+		System.out.println("문자를 입력하세요.");
+		char input = scan.next().charAt(0);
+		
+		for(int i =0; i<charArray.length; i++) {
+			if(input == charArray[i]) {
+				System.out.println("문자의 위치 : " + i + "번");
+			}
+			else
+			count++;
+		}
+		if(count >= charArray.length)
+			System.out.println("none");
+
 	}
 
 }
